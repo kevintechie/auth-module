e2fae661f94fcdf2496f402c71fe157ed76b7f2e
Kevin Coleman <kevin@kevintechie.com>
2019-11-13 17:29:39 -0800

    switch

--- examples/demo/nuxt.config.js
+++ examples/demo/nuxt.config.js
@@ -34,15 +34,15 @@ module.exports = {
       shibboleth: {
         _scheme: 'oauth2',
         authorization_endpoint: 'https://fosterstage.auth.us-west-2.amazoncognito.com/oauth2/authorize',
-        // userinfo_endpoint: 'https://r4cq2hhfv4.execute-api.us-west-1.amazonaws.com/latest/userInfo',
-        userinfo_endpoint: 'https://fosterstage.auth.us-west-2.amazoncognito.com/oauth2/userInfo',
+        userinfo_endpoint: 'https://r4cq2hhfv4.execute-api.us-west-1.amazonaws.com/latest/userInfo',
+        // userinfo_endpoint: 'https://fosterstage.auth.us-west-2.amazoncognito.com/oauth2/userInfo',
         scope: ['email', 'openid', 'profile', 'aws.cognito.signin.user.admin'],
         access_token_endpoint: 'https://fosterstage.auth.us-west-2.amazoncognito.com/oauth2/token',
         response_type: 'code',
         token_type: 'Bearer',
         redirect_uri: 'http://localhost:3000/callback',
         client_id: '3nic7mc5dimiu0l35d1glpqnor',
-        token_key: 'access_token',
+        token_key: 'id_token',
         grant_type: 'authorization_code'
       }
     }
